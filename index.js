cf383a526a22edef37a002b2f86f55dbb448b513
ShaileshRimal <127722077+ShaileshRimal@users.noreply.github.com>
2023-05-09 20:29:20 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let display=document.getElementsByClassName("display")[0];
 let expression=display.value;
 let nums=document.getElementsByClassName("digits");
-for (let i = 0; i < 15; i++) {
+for (let i = 0; i < 16; i++) {
     nums[i].addEventListener("click", ()=>{
         expression=display.value;
         if(nums[i].innerHTML=== '='){
