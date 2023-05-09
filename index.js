48f5e352bf297a215e8532cc0aea56c8815355fa
ShaileshRimal <127722077+ShaileshRimal@users.noreply.github.com>
2023-05-09 16:57:13 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let display=document.getElementsByClassName("display")[0];
+let expression=display.value;
+let nums=document.getElementsByClassName("digits");
+for (let i = 0; i < 15; i++) {
+    nums[i].addEventListener("click", ()=>{
+        expression=display.value;
+        if(nums[i].innerHTML=== '='){
+            display.value=eval(expression);
+            expression=display.value
+
+        }
+        else{
+            expression+=nums[i].innerHTML;
+        display.value=expression;
+        }
+        
+        });
+}
