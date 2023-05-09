886c11b4271c2c528f76875f59c10d560769e3a5
ShaileshRimal <127722077+ShaileshRimal@users.noreply.github.com>
2023-05-09 21:53:49 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+let display = document.getElementsByClassName("display")[0];
+let expression = display.value;
+let nums = document.getElementsByClassName("digits");
+for (let i = 0; i < 17; i++) {
+    nums[i].addEventListener("click", () => {
+        expression = display.value;
+        if (nums[i].innerHTML === "AC") {
+            display.value = "";
+            expression = "";
+            display.focus();
+        }
+        else if (nums[i].innerHTML === '=') {
+            display.value = eval(expression);
+            expression = display.value
+            display.focus();
+        }
+
+        else {
+            expression += nums[i].innerHTML;
+            display.value = expression;
+            display.focus();
+        }
+
+
+    });
+}
